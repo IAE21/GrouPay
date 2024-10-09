ed3218309830ac4734f0e6442fb144cee7e81520
throdahl <129460856+throdahl@users.noreply.github.com>
2024-10-08 22:50:51 -0400

    Update tables.py
    Having the same problems on my machine as with the setup.py. This seems to work on mine though. Again not sure why

--- tables.py
+++ tables.py
@@ -1,12 +1,15 @@
 from mysql.connector import connect
 
 
-with connect(
+conn = connect(
         host='localhost',
         user='root',
         passwd='mysql123',
         database='groupay',
-) as conn:
+        auth_plugin='mysql_native_password'
+)
+try:
+    cur = conn.cursor()
     db_create_groups = """CREATE TABLE IF NOT EXISTS BILLGROUPS(
                             Group_num INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
                             Amount INT NOT NULL
@@ -16,6 +19,10 @@ with connect(
                             Group_number INT NOT NULL AUTO_INCREMENT,
                             FOREIGN KEY (Group_number) REFERENCES BILLGROUPS(Group_num)
                             )"""
-    with conn.cursor() as cur:
-        cur.execute(db_create_groups)
-        cur.execute(db_create_users)
+    cur.execute(db_create_groups)
+    cur.execute(db_create_users)
+    conn.commit()
+
+finally:
+    cur.close()
+    conn.close()
