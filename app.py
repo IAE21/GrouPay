ec92e2508c4175dc9638fc9f1a1a93b87d628429
Will Hudmon <111246902+Hudbone@users.noreply.github.com>
2024-10-09 21:54:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from flask import Flask, render_template
+from flask_mysqldb import MySQL
 app = Flask(__name__)
+
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "mysql123"
+app.config["MYSQL_DB"] = "groupay"
+
+mysql = MySQL(app)
 
 @app.route('/')
 def home():
