6c7c55b9bfe14f77c02c4886e937e456024c14cb
Ian Estevez <iae21@fsu.edu>
2024-11-08 15:14:13 -0500

    Created pays_for table within db and restructured other tables in db accordingly, created a folder for Increment 1 documentation.

--- setup.py
+++ setup.py
@@ -24,20 +24,29 @@ try:
                             username VARCHAR(50) NOT NULL,
                             password VARCHAR(255) NOT NULL,
                             corporate INT NOT NULL,
-                            group_num INT,
-                            FOREIGN KEY (group_num) REFERENCES BILL_GROUPS(group_num) ON DELETE CASCADE ON UPDATE CASCADE,
                             UNIQUE KEY uname (username)
                             )"""
         db_create_groups = """CREATE TABLE IF NOT EXISTS BILL_GROUPS(
                             group_num INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                            group_name VARCHAR(100),
+                            company VARCHAR(150),
                             amount DEC(10,2) NOT NULL,
                             member_ids VARCHAR(500)
+                            )"""
+        db_create_pays = """CREATE TABLE IF NOT EXISTS PAYS_FOR(
+                            user_id INT NOT NULL,
+                            group_num INT NOT NULL,
+                            percent DEC(4,2) NOT NULL,
+                            FOREIGN KEY (user_id) REFERENCES USERS(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
+                            FOREIGN KEY (group_num) REFERENCES BILL_GROUPS(group_num) ON DELETE CASCADE ON UPDATE CASCADE,
+                            PRIMARY KEY (user_id, group_num)
                             )"""
         db_admin = """CREATE USER IF NOT EXISTS 'admin'@'localhost' IDENTIFIED BY 'admin123';"""
         db_grant = "GRANT INSERT, UPDATE, DELETE, ALTER, SELECT ON *.* TO 'admin'@'localhost';"
         with conn.cursor() as cur:
             cur.execute(db_create_groups)
             cur.execute(db_create_users)
+            cur.execute(db_create_pays)
             cur.execute(db_admin)
             cur.execute(db_grant)
         conn.commit()
