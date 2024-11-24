9484b059006b77715c65a2996b6fc2d74d23cd79
throdahl <129460856+throdahl@users.noreply.github.com>
2024-11-24 15:45:00 -0500

    Create group invites table

--- setup.py
+++ setup.py
@@ -43,14 +43,34 @@ try:
                             PRIMARY KEY (user_id, group_num)
                             )"""
         db_create_friend_requests = """CREATE TABLE IF NOT EXISTS FRIEND_REQUESTS(
-                                    requester_id INT NOT NULL,
-                                    requestee_id INT NOT NULL,
+                                        requester_id INT NOT NULL,
+                                        requestee_id INT NOT NULL,
+                                        status VARCHAR(20) NOT NULL DEFAULT 'pending',
+                                        request_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                        PRIMARY KEY (requester_id, requestee_id),
+                                        FOREIGN KEY (requester_id) REFERENCES USERS(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
+                                        FOREIGN KEY (requestee_id) REFERENCES USERS(user_id) ON DELETE CASCADE ON UPDATE CASCADE
+                                        )"""
+        db_create_friends = """CREATE TABLE IF NOT EXISTS FRIENDS(
+                                user_id INT NOT NULL,
+                                friend_id INT NOT NULL,
+                                friendship_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                PRIMARY KEY (user_id, friend_id),
+                                FOREIGN KEY (user_id) REFERENCES USERS(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
+                                FOREIGN KEY (friend_id) REFERENCES USERS(user_id) ON DELETE CASCADE ON UPDATE CASCADE
+                                )"""
+        db_create_group_invites = """CREATE TABLE IF NOT EXISTS GROUP_INVITES(
+                                    group_num INT NOT NULL,
+                                    sender_id INT NOT NULL,
+                                    receiver_id INT NOT NULL,
                                     status VARCHAR(20) NOT NULL DEFAULT 'pending',
-                                    request_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                                    PRIMARY KEY (requester_id, requestee_id),
-                                    FOREIGN KEY (requester_id) REFERENCES USERS(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
-                                    FOREIGN KEY (requestee_id) REFERENCES USERS(user_id) ON DELETE CASCADE ON UPDATE CASCADE
+                                    invite_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                    PRIMARY KEY (group_num, receiver_id),
+                                    FOREIGN KEY (group_num) REFERENCES BILL_GROUPS(group_num) ON DELETE CASCADE ON UPDATE CASCADE,
+                                    FOREIGN KEY (sender_id) REFERENCES USERS(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
+                                    FOREIGN KEY (receiver_id) REFERENCES USERS(user_id) ON DELETE CASCADE ON UPDATE CASCADE
                                     )"""
+        
         db_admin = """CREATE USER IF NOT EXISTS 'admin'@'localhost' IDENTIFIED BY 'admin123';"""
         db_grant = "GRANT INSERT, UPDATE, DELETE, ALTER, SELECT ON *.* TO 'admin'@'localhost';"
         with conn.cursor() as cur:
@@ -60,6 +80,8 @@ try:
             cur.execute(db_admin)
             cur.execute(db_grant)
             cur.execute(db_create_friend_requests)
+            cur.execute(db_create_friends)
+            cur.execute(db_create_group_invites)
         conn.commit()
 except Error as e:
     print(e)
